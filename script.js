4b272af4c77e079ef7635479f3b56ded84f72df2
TavisYearwood1 <ta313579@ucf.edu>
2026-03-03 22:25:56 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const searchBtn = document.getElementById("searchBtn");
+const pokemonInput = document.getElementById("pokemonInput");
+const pokemonDisplay = document.getElementById("pokemonDisplay");
+const teamDisplay = document.getElementById("teamDisplay");
+
+const cache = {};
